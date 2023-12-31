78ab72cbe0f1971e38e2dca5a6f3c8e0f618feec
skPaik <skPaik@skPaik-Desktop>
2023-12-31 11:05:08 +0900

    - 인터셉터 추가 - 추가만 했을뿐 구현은 미완료

---
+++ src/main/java/org/study/blog/common/interceptor/Interceptor.java
@@ -0,0 +1,56 @@
+/**
+ * @projectName LegacyBlog
+ * @version 1.0
+ * @author skPaik
+ * @date 2023. 12. 31.
+ */
+package org.study.blog.common.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @className Interceptor.java
+ * @description 인터셉터
+ */
+public class Interceptor implements HandlerInterceptor {
+
+	/**
+	 * @see org.springframework.web.servlet.HandlerInterceptor#preHandle(javax.servlet.http.HttpServletRequest,
+	 *      javax.servlet.http.HttpServletResponse, java.lang.Object)
+	 */
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		System.out.println("preHandel");
+		return HandlerInterceptor.super.preHandle(request, response, handler);
+	}
+
+	/**
+	 * @see org.springframework.web.servlet.HandlerInterceptor#postHandle(javax.servlet.http.HttpServletRequest,
+	 *      javax.servlet.http.HttpServletResponse, java.lang.Object,
+	 *      org.springframework.web.servlet.ModelAndView)
+	 */
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		System.out.println("postHandle");
+		HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+	}
+
+	/**
+	 * @see org.springframework.web.servlet.HandlerInterceptor#afterCompletion(javax.servlet.http.HttpServletRequest,
+	 *      javax.servlet.http.HttpServletResponse, java.lang.Object,
+	 *      java.lang.Exception)
+	 */
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		System.out.println("afterCompletion");
+		HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+	}
+
+}
